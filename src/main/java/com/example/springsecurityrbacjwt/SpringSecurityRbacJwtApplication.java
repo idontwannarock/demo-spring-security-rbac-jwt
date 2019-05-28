3133545dfe69075c0020b219dc596fca18fad715
Howard Wang <idontwannarock@gmail.com>
2019-05-28 09:47:45 +0800

    Init project

---
+++ src/main/java/com/example/springsecurityrbacjwt/SpringSecurityRbacJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityrbacjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRbacJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRbacJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityrbacjwt/SpringSecurityRbacJwtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springsecurityrbacjwt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityRbacJwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
